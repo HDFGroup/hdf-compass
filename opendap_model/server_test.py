eca7ec295a99d7ebf09cdc94794388461b1fa0ba
Matt Comerford <matthew.comerford@colorado.edu>
2014-08-05 10:47:32 -0600

    prototype for OpENDAP server plugin

--- opendap_model/server_test.py
+++
@@ -1,7 +0,0 @@
-from pydap.client import open_url
-from pydap.model import *
-from pydap.proxy import ArrayProxy
-
-dataset = open_url('http://test.opendap.org/dap/data/nc/coads_climatology.nc')
-
-print dataset.values()
