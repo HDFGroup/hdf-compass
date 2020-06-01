e28338608be08870634508de7f5ce6fea0cba873
giumas <giuseppemasetti@gmail.com>
2020-06-01 20:56:24 +0200

    fixing PyCharm namespace issue

--- hdf_compass/__init__.py
+++ hdf_compass/__init__.py
@@ -1,9 +1,1 @@
-"""
-HDFCompass Namespace
-"""
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
