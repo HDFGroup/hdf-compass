9dbd8ce30c84254c87940024c1bcf670e53d1ce2
giumas <giuseppemasetti@gmail.com>
2022-03-27 17:54:34 +0200

    improved logging messages

--- HDFCompass.py
+++ HDFCompass.py
@@ -10,27 +10,12 @@
 # request a copy from help@hdfgroup.org.                                     #
 ##############################################################################
 import logging
-
-
-class LoggingFilter(logging.Filter):
-    """ An example of logging filter that disables the logging from a specific module """
-    def filter(self, record):
-        # print(record.name)
-        if record.name.startswith('hdf_compass.compass_viewer.info'):
-            return False
-        return True
-
-
-# logging settings
-logger = logging.getLogger()
-logger.setLevel(logging.NOTSET)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)  # change to WARNING to minimize verbosity, DEBUG for high verbosity
-ch_formatter = logging.Formatter('%(levelname)-7s %(name)s.%(funcName)s:%(lineno)d > %(message)s')
-ch.setFormatter(ch_formatter)
-# ch.addFilter(LoggingFilter())  # uncomment to activate the logging filter
-logger.addHandler(ch)
-
 from hdf_compass import compass_viewer
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)-7s %(name)s.%(funcName)s:%(lineno)d > %(message)s'
+)
+logging.getLogger("hdf_compass").setLevel(logging.DEBUG)  # INFO to minimize verbosity, DEBUG for higher verbosity
 
 compass_viewer.run()
